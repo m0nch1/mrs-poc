32638653604e8d72bff3b6765e6bd3b2391cf8c6
m0nch1 <yuta.kato@0004s.com>
2022-06-04 14:18:44 +0900

    chore: change root dir & and sample api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  rootDir: "src",
   typescript: {
-    strict: true
-  }
-})
+    strict: true,
+  },
+});
