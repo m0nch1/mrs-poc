ccedf1ab274732df363ecfe8f6ea7a799e145f1f
m0nch1 <yuta.kato@0004s.com>
2022-06-04 15:12:12 +0900

    何となくページを分けた

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
-    <h1>ユーザー覧</h1>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user.username }}
-      </li>
-    </ul>
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <NuxtPage />
   </div>
 </template>
-
-<script setup lang="ts">
-import type { User } from "~/types/api/users";
-const { data: users } = await useFetch<User[]>("/api/users");
-</script>
---
+++ src/pages/users/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>ユーザー覧</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.username }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { User } from "~/types/api/users";
+const { data: users } = await useFetch<User[]>("/api/users");
+</script>
