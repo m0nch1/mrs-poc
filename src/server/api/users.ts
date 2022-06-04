fb08a1061e67774117d9d50d43d2c64ec179e173
m0nch1 <yuta.kato@0004s.com>
2022-06-04 17:56:29 +0900

    fix: delete console log

--- src/server/api/users.ts
+++ src/server/api/users.ts
@@ -3,7 +3,6 @@ import type { User } from "~/types/api/users";
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
   const result: User[] = await $fetch("http://localhost:5000/users");
-  console.log(result);
 
   return result;
 };
