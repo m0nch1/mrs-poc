f6a36bbcfb1a7f6299cd31b8b792803758a75e18
m0nch1 <yuta.kato@0004s.com>
2022-08-28 23:20:00 +0900

    feat: 会議室一覧を取れるようにした

--- src/utils/google/auth.ts
+++ src/utils/google/auth.ts
@@ -4,18 +4,28 @@ dotenv.config();
 
 const GOOGLE_PRIVATE_KEY = process.env.private_key;
 const GOOGLE_CLIENT_EMAIL = process.env.client_email;
+const GOOGLE_ADMIN_ID = process.env.admin_email;
 export const GOOGLE_CALENDAR_ID = process.env.calendar_id;
-const SCOPES = ["https://www.googleapis.com/auth/calendar"];
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar",
+  "https://www.googleapis.com/auth/admin.directory.resource.calendar",
+];
 
 const jwtClient = new google.auth.JWT(
   GOOGLE_CLIENT_EMAIL,
   "./credentials/keys.json",
   GOOGLE_PRIVATE_KEY,
-  SCOPES
+  SCOPES,
+  GOOGLE_ADMIN_ID
 );
 
 export const calendarClient = google.calendar({
   version: "v3",
+  auth: jwtClient,
+});
+
+export const adminClient = google.admin({
+  version: "directory_v1",
   auth: jwtClient,
 });
 
